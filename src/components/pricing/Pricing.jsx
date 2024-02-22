972b556d1a555278ea718ac92ba3d792f798e554
Apeloko Timilehin <110821276+haypeks@users.noreply.github.com>
2024-02-22 21:27:04 +0000

    minor changes

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -12,41 +12,7 @@ const Pricing = () => {
           <p className="pricing__price">
             <span>$0</span> per month
           </p>
-          <div className="pricing__props">
-            <p>
-              <IoMdCheckmark className="pricing__checkmark" /> LLM Agnostic
-              Model
-            </p>
-            <p>
-              <IoMdCheckmark className="pricing__checkmark" /> 10,000words/month
-            </p>
-            <p>
-              <IoMdCheckmark className="pricing__checkmark" /> AI Article Writer
-              4.o
-            </p>
-            <p>
-              <IoMdCheckmark className="pricing__checkmark" /> Complete Article
-              Rewriter
-            </p>
-            <p>
-              <IoMdCheckmark className="pricing__checkmark" /> 30 Languages
-            </p>
-            <p>
-              <IoMdCheckmark className="pricing__checkmark" /> 100+ AI Templates
-            </p>
-            <p>
-              <IoMdCheckmark className="pricing__checkmark" /> ChatGPT-like
-              chatbot
-            </p>
-            <button type="button">Get Started</button>
-          </div>
-        </div>
 
-        <div className="pricing__card pro">
-          <h2>Pro</h2>
-          <p className="pricing__price">
-            <span>$5</span> per month
-          </p>
           <div className="pricing__props">
             <p>
               <IoMdCheckmark className="pricing__checkmark" /> LLM Agnostic
@@ -77,6 +43,41 @@ const Pricing = () => {
           </div>
         </div>
 
+        <div className="pricing__card pro">
+          <h2>Pro</h2>
+          <p className="pricing__price">
+            <span>$5</span> per month
+          </p>
+          <div className="pricing__props">
+            <p>
+              <IoMdCheckmark className="pricing__checkmark" /> GPT 3.5 Model
+            </p>
+            <p>
+              <IoMdCheckmark className="pricing__checkmark" />{" "}
+              100,000words/month
+            </p>
+            <p>
+              <IoMdCheckmark className="pricing__checkmark" /> AI Article Writer
+              4.o
+            </p>
+            <p>
+              <IoMdCheckmark className="pricing__checkmark" /> Higher quality
+              image
+            </p>
+            <p>
+              <IoMdCheckmark className="pricing__checkmark" /> API Access
+            </p>
+            <p>
+              <IoMdCheckmark className="pricing__checkmark" /> Priority support
+            </p>
+            <p>
+              <IoMdCheckmark className="pricing__checkmark" />
+              Bulk Processing
+            </p>
+            <button type="button">Get Started</button>
+          </div>
+        </div>
+
         <div className="pricing__card unlimited">
           <h2>Unlimited</h2>
           <p className="pricing__price">
@@ -84,29 +85,27 @@ const Pricing = () => {
           </p>
           <div className="pricing__props">
             <p>
-              <IoMdCheckmark className="pricing__checkmark" /> LLM Agnostic
-              Model
+              <IoMdCheckmark className="pricing__checkmark" /> GPT4.0 Model
             </p>
             <p>
-              <IoMdCheckmark className="pricing__checkmark" /> 10,000words/month
+              <IoMdCheckmark className="pricing__checkmark" /> Unlimited
+              words/month
             </p>
             <p>
-              <IoMdCheckmark className="pricing__checkmark" /> AI Article Writer
-              4.o
+              <IoMdCheckmark className="pricing__checkmark" /> Bulk Processing
             </p>
             <p>
-              <IoMdCheckmark className="pricing__checkmark" /> Complete Article
-              Rewriter
+              <IoMdCheckmark className="pricing__checkmark" /> Training sessions
             </p>
             <p>
-              <IoMdCheckmark className="pricing__checkmark" /> 30 Languages
+              <IoMdCheckmark className="pricing__checkmark" /> 50 Languages
             </p>
             <p>
-              <IoMdCheckmark className="pricing__checkmark" /> 100+ AI Templates
+              <IoMdCheckmark className="pricing__checkmark" /> Premium support
             </p>
             <p>
-              <IoMdCheckmark className="pricing__checkmark" /> ChatGPT-like
-              chatbot
+              <IoMdCheckmark className="pricing__checkmark" /> Workspace
+              analytics
             </p>
             <button type="button">Buy Now</button>
           </div>
